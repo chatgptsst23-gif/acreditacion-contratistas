ba21d71d6c207b3936ae232cb770ef82afb42956
JosueAuris <chatgptsst23@gmail.com>
2026-09-22 12:55:38 -0500

    Update config.js

--- sitio/config.js
+++ sitio/config.js
@@ -3,6 +3,6 @@
    Si queda vacía, la página funciona en modo demostración y guarda todo en el navegador.
    PILOTO: true muestra el aviso "usa solo datos ficticios". Cámbialo a false al pasar a producción. */
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxQVQbcZSN90fNpZ7HEnax2-pDgtV32ial2z-21W-jScaKYXbSr_Cw3nnWNbdLmyipJLw/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbzZLs4HoQ3K_J-fHLojAN1t796V7I24xpQvICxUn4_-YbTCQhxNHyj6c6R_WNwOm2FjJw/exec",
   PILOTO: true
 };
